a1fa1de8527d50f3b738e584a980eb94896223ea
David <45155292+DavoDC@users.noreply.github.com>
2023-03-21 22:28:47 +0800

    Basic path reading

--- ReplaysAnalyser.cpp
+++ ReplaysAnalyser.cpp
@@ -2,9 +2,33 @@
 // This file contains the 'main' function. 
 // Program execution begins and ends there.
 
+#include <string>
 #include <iostream>
+#include <filesystem>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	// Start message 
+	cout << "\n####### WELCOME TO REPLAYS ANALYSER \n";
+
+	// Set path to read from
+	string path = "SAMPLE_DATA";
+
+	// For every path in the directory
+	for (const auto& curPath : filesystem::recursive_directory_iterator(path)) {
+
+		// Convert to string
+		string curPathS = curPath.path().generic_string();
+
+		// If doesn't have dot/ext, add space  (Separates folders)
+		if (!strstr(curPathS.c_str(), ".")) {
+			cout << "\n" << endl;
+		}
+
+		// Print 
+		cout << curPathS << endl;
+	}
+
 }
