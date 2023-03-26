95e17fb4486209b606490bae185337539a6eb6cd
David <45155292+DavoDC@users.noreply.github.com>
2023-03-27 07:34:38 +0800

    Minor update
    - Removed extra comments, cleaned up Analyser
    - Added more ideas

--- Code/ReplaysAnalyser.cpp
+++ Code/ReplaysAnalyser.cpp
@@ -8,12 +8,15 @@
 // Include needed header
 #include "MatchList.h"
 
-// Libraries
+// ### Libraries
+// # For set
 #include <set>
+// # For function type
 #include <functional>
 
 // Namespace mods
 using namespace std;
+
 
 // ### Global variables
 
@@ -26,27 +29,8 @@ const string replayPath = "C:/Users/David/GitHubRepos/SSF2ReplaysCOPY";
 //'C:/Users/David/SSF2Replays'
 
 
-
 // Declarations
-void printPercentageBreakdown(set<string> uniques, vector<string> all);
-void printPercentageBreakdown(vector<Match> matchV, function<string(Match)> func);
-
-//// TEST
-//#include <functional>
-//// Function that takes a pointer
-//// to a function
-//int invoke(int x, int y, function<int(int, int)> func)
-//{
-//	return func(x, y);
-//}
-//
-//// Function that takes a pointer
-//// to a function
-//string invokeS(Match m, function<string(Match)> func)
-//{
-//	return func(m);
-//}
-
+void printFreqStats(vector<Match> matchV, function<string(Match)> func);
 
 
 
@@ -59,7 +43,6 @@ int main()
 
 	// Parse matches into list of matches
 	MatchList ml = MatchList(replayPath);
-	int matchCount = ml.getSize();
 	print("\nParsed " + ml.getSizeS() + " matches!");
 
 	// Sort by date
@@ -70,65 +53,16 @@ int main()
 
 	// ### Version Stats
 	print("\n### Version Stats ###");
-	printPercentageBreakdown(ml.getMatches(), [](Match m) -> string {
+	printFreqStats(ml.getMatches(), [](Match m) -> string {
 		return m.getVersion();
 		});
 
-	/*;
-	set<string> uniqVersions;
-	vector<string> allVersions;
-	for (Match curMatch : ml.getMatches())
-	{
-		string curVersion = curMatch.getVersion();
-		uniqVersions.insert(curVersion);
-		allVersions.push_back(curVersion);
-	}
-	printPercentageBreakdown(uniqVersions, allVersions);*/
 
-
-	
 	// ### Year Stats
 	print("\n### Year Stats ###");
-	printPercentageBreakdown(ml.getMatches(), [](Match m) -> string {
+	printFreqStats(ml.getMatches(), [](Match m) -> string {
 		return to_string(m.getYear());
 		});
-	/*
-	set<string> years;
-	vector<string> allYears;
-	for (Match curMatch : ml.getMatches())
-	{
-		string curVersion = to_string(curMatch.getYear());
-		years.insert(curVersion);
-		allYears.push_back(curVersion);
-	}
-	printPercentageBreakdown(years, allYears);*/
-
-
-
-	//Match m = ml.getMatches()[0];
-	
-	
-
-	// Perform Addition
-	/*print("Addition of 20 and 10 is ");
-	int k = invoke(20, 10,
-		[](int x,
-			int y) -> int {
-				return x + y;
-		});
-	print(to_string(k), true);
-
-
-	Match m = ml.getMatches()[0];
-	print("The version of this match object is ");
-	string s = invokeS(m,
-		[](Match m) -> string {
-				return m.getVersion();
-		});
-	print(s);*/
-
-
-	
 
 	// END
 	print("\n");
@@ -137,9 +71,13 @@ int main()
 
 
 
-void printPercentageBreakdown(vector<Match> matchV, function<string(Match)> func)
+
+// Print frequency statistics for a given property
+// matchV: List of matches
+// func: Function that retrieves property
+void printFreqStats(vector<Match> matchV, function<string(Match)> func)
 {
-	// Unique items
+	// A set containing each variant (unique items)
 	set<string> variants;
 
 	// All items
@@ -156,32 +94,21 @@ void printPercentageBreakdown(vector<Match> matchV, function<string(Match)> func
 		all.push_back(curItem);
 	}
 
-	// Pass to helper
-	printPercentageBreakdown(variants, all);
-}
-
-
-
-// Print each version and percentage
-// uniques: A set containing each variant
-// all: All the items
-void printPercentageBreakdown(set<string> uniques, vector<string> all)
-{
 	// Total items
 	int total = int(all.size());
 
 	// For each variant
-	for (string curV : uniques)
+	for (string curV : variants)
 	{
 		// Find out how many instances of this variant are present
 		int freq = int(count(all.begin(), all.end(), curV));
 		string freqS = to_string(freq);
 
-		// Calculate a percentage out of the total items
-		int percentage = int(round(((double)freq / (double) total) * 100));
+		// Calculate as a percentage out of the total items
+		int percentage = int(round(((double)freq / (double)total) * 100));
 		string percentageS = to_string(percentage);
 
-		// Final
+		// Format nicely
 		print(format("{} = {} matches ({}%)", curV, freqS, percentageS));
 	}
 }
